0652167c8df76a637d59eb21c53c9bcd1fcc9c45
IYER ARVIND SUBRAMANYAM <arvind.cooliyer@gmail.com>
2025-09-15 22:33:24 +0530

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,8 +1,7 @@
 def insert(a,element):
-    element = int(input("Enter an element to be inserted"))
     hashval = element % len(a)
     for i in range(len(a)):
-        index = (hashval +i)%10
+        index = (hashval +i)%len(a)
         if (a[index] == None) :
             a[index] = element
             print(f"Element has been inserted at {index}")
@@ -11,15 +10,15 @@ def insert(a,element):
 
 
 def search(a,key):
-   hashval = element % len(a)
-   for i in rangeof len(a):
-    index = (hashval +i)%10
+   hashval = key % len(a)
+   for i in range(len(a)):
+    index = (hashval +i)% len(a)
     if (a[index] == None):
             break
-        if a[index] == key:
+    if a[index] == key:
             print(f"Element {key} found at index {index}")
             return
-    print("Element not found after searching")
+   print("Element not found after searching")
     
 
 
@@ -35,13 +34,19 @@ def delete(a, key):
             return
     print("Element not found ")
 
-a = [None] * len(a)
+def display(a):
+    print("Current Hashtable :")
+    for i,value in enumerate(a):
+        print(f"{i} : {value}")
+
+a = [None] * 10
 
 while True :
     print (''' 1 : Insert 
                 2 : Search 
                 3 : Delete 
-                4 : Exit ''')
+                4 : Display
+                5 : Exit ''')
 
     choice = int(input("Enter the operation to be performed :"))
     if(choice == 1):
@@ -57,7 +62,10 @@ while True :
         delete(a,element)
 
     elif(choice == 4):
-        break 
+        display(a)
+    
+    elif(choice == 5):
+        break
     
     else :
         print("Please enter a valid operation to be performed ")
@@ -65,6 +73,7 @@ while True :
 
 
     
+
 
 
 
